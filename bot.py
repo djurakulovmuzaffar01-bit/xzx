cf1a82012fcabec22816ed5be2f7ca97649a9ddf
djurakulovmuzaffar01-bit <djurakulovmuzaffar01@gmail.com>
2025-10-21 23:19:07 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
-import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from aiogram.types import WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import CommandStart
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+import asyncio
 
-TOKEN = "8080590843:AAEvo9MygGH8RN1axbsnmzSIFzbaK_dxx0A"
-
+TOKEN = "BOT_TOKENINGNI_BU_YERGA_QO'Y"  # @BotFather bergan tokenni bu yerga yoz
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command("start"))
-async def start(message: types.Message):
+@dp.message(CommandStart())
+async def start_handler(message: types.Message):
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="Open Mini App 🌐", web_app=WebAppInfo(url="https://yourdomain.com"))]
+            [InlineKeyboardButton(
+                text="🌐 Open Mini App",
+                web_app=WebAppInfo(url="https://djurakulovmuzaffar01-bit.github.io/xzx/")  # GitHub Pages manziling
+            )]
         ]
     )
-    await message.answer("Salom jigar! Mini appni och 👇", reply_markup=keyboard)
+    await message.answer("Salom jigar! Mini Appni och 👇", reply_markup=keyboard)
 
 async def main():
     await dp.start_polling(bot)
